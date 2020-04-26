ab58aba0e525e134b10008c2ce9ddcdb0514a5fe
huangmei123 <1593702111@qq.com>
2020-04-26 10:06:18 +0800

    网易严选

--- wangyi-shop/src/pages/index/index.vue
+++ wangyi-shop/src/pages/index/index.vue
@@ -40,6 +40,52 @@
         </div>
       </div>
     </div>
+    <!-- 新品发布 -->
+    <div class="newgoods">
+      <div class="newgoods-top" @click="goodsList('new')">
+        <div class="top">
+          <p>新品首发</p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in newGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <!-- 人气推荐 -->
+    <div class="newgoods hotgoods">
+      <div class="newgoods-top" @click="goodsList('hot')">
+        <div class="top">
+          <p>
+            人气推荐
+            <span></span>
+            好物精选
+          </p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in hotGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -52,7 +98,9 @@ export default {
     return{
       banner:[],
       channel:[],
-      brandList:[]
+      brandList:[],
+      newGoods:[],
+      hotGoods:[]
     }
   },
   computed:{
@@ -116,6 +164,8 @@ export default {
       this.banner = data.banner
       this.channel=data.channel
       this.brandList = data.brandList
+      this.newGoods = data.newGoods
+      this.hotGoods = data.hotGoods
     },
     categroyList (id) {
       console.log(123)
@@ -123,6 +173,28 @@ export default {
         url: '/pages/categroylist/main?id=' + id
       })
     },
+    branddetail (id) {
+      wx.navigateTo({
+        url: '/pages/branddetail/main?id=' + id
+      })
+    },
+    tobrandList () {
+      wx.navigateTo({
+        url: '/pages/brandlist/main'
+      })
+    },
+    goodsList (info) {
+      if (info == 'hot') {
+        wx.navigateTo({
+          url: '/pages/newgoods/main?isHot=' + 1
+        })
+      } else {
+        wx.navigateTo({
+          url: '/pages/newgoods/main?isNew=' + 1
+        })
+      }
+    },
+
   }
 }
 </script>
