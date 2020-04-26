00ab30e6eebabe9b32551a9dfb463368240f09f5
huangmei123 <1593702111@qq.com>
2020-04-26 10:04:11 +0800

    node端

--- wangyi-shop-node/controllers/home/index.js
+++ wangyi-shop-node/controllers/home/index.js
@@ -11,10 +11,17 @@ module.exports= async(ctx) =>{
     const brandList = await mysql('nideshop_brand').where({
       is_new: 1
     }).orderBy('new_sort_order', 'asc').limit(4).select()
-
+    // 新品首发
+    const newGoods = await mysql('nideshop_goods').whereIn('id', [1181000, 1135002, 1134030, 1134032]).andWhere('is_new', 1).select()
+    // 人气推荐
+    const hotGoods = await mysql('nideshop_goods').column('id', 'name', 'list_pic_url', 'retail_price', 'goods_brief').where({
+      is_hot: 1
+    }).limit(5).select()
     ctx.body = {
       'banner': banner,
       'channel':channel,
-      'brandList':brandList
+      'brandList':brandList,
+      'newGoods':newGoods,
+      'hotGoods':hotGoods
     }
 }
